8c2f99e1feb12978408dadf73285d0f27927f124
Peder Korsveien <peder.korsveien@gmail.com>
2017-03-05 23:39:40 +0100

    Test transactionParser

--- frontend/tests/Tests.elm
+++ frontend/tests/Tests.elm
@@ -4,14 +4,28 @@ import Test exposing (..)
 import Expect
 import Date exposing (Month(..))
 import Date.Extra as DateExtra
-import Commands exposing (transactionsDecoder)
+import Commands exposing (transactionDecoder)
+import Json.Decode as JD
+import Model exposing (Transaction)
 
 
 all : Test
 all =
-    describe "Decoding"
-        [ test "Decode transaction"
+    describe "JSON decoders"
+        [ test "Decoding transaction type"
             <| \() ->
-                Expect.equal (JD.decodeString transactionsDecoder "[{\"id\": \"1\", \"date\": \"2017-03-05 00:00:00Z\", \"category\":\"Kiosk\", \"amount\": \"100.20\"}]")
-                    (Ok (Transaction (DateExtra.fromSpec utc noTime 2017 Mar 5)))
+                Expect.equal
+                    (JD.decodeString transactionDecoder
+                        "{\"id\": \"1\", \"date\": \"2017-03-05 00:00:00Z\", \"category\":\"Kiosk\", \"amount\": 100.20}"
+                    )
+                    (Ok
+                        (Transaction "1"
+                            (DateExtra.fromSpec DateExtra.utc
+                                (DateExtra.atTime 0 0 0 0)
+                                (DateExtra.calendarDate 2017 Mar 5)
+                            )
+                            "Kiosk"
+                            100.2
+                        )
+                    )
         ]
