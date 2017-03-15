11c139d173f892cc7b077fa8fcba793282f11d38
Peder Korsveien <peder.korsveien@gmail.com>
2017-03-15 21:37:22 +0100

    Add skeleton.css

--- static/index.js
+++ static/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 require('./index.html');
+require('./normalize.css');
+require('./skeleton.css');
 require('./style.css');
 const Elm = require('../src/Main');
 const app = Elm.Main.fullscreen();
